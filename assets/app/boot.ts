078f7a9cfae87093007c83190a12b39815b8f423
pwbwolf <pwborodich@gmail.com>
2016-06-29 13:56:46 -0400

    grammer changes

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -3,6 +3,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
 
 bootstrap(AppComponent, [
     APP_ROUTER_PROVIDERS
